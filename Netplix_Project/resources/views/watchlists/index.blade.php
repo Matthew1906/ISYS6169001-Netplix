5f8f37086f56211b4cbb22a500e1a4eb106dee1d
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2021-12-09 13:39:27 +0700

    fixed watchlist view styles

--- Netplix_Project/resources/views/watchlists/index.blade.php
+++ Netplix_Project/resources/views/watchlists/index.blade.php
@@ -3,31 +3,32 @@
     <link rel="stylesheet" href="{{ asset('css/watchlist.css') }}">
 @endsection
 @section('content')
-    <div class="title-section container row">
+<div class="watchlist">
+    <div class="title-section cust-container cust-row">
         <i class="fa fa-bookmark bookmark-icon text--red"></i>
-        <h1 class="row">My<span class="text--red">Watchlist</span></h1>
+        <h1 class="cust-row">My<span class="text--red">Watchlist</span></h1>
     </div>
 
-    <div class="search-section container">
-        <div class="search-section__bar row">
+    <div class="search-section cust-container cust-row">
+        <div class="search-section__bar cust-row">
             <input type="text" name="" id="" placeholder="Search your watchlist..." class="search-section__input">
             <i class="fa fa-search search-icon"></i>
         </div>
-        <div class="search-section__filter row">
+        <div class="search-section__filter cust-row">
             <i class="fa fa-sliders slider-icon"></i>
             <h4>Filter</h4>
         </div>
     </div>
 
-    <div class="watchlist-section container col">
-        <div class="watchlist-title row">
+    <div class="watchlist-section cust-container cust-col">
+        <div class="watchlist-title cust-row">
             <h4 class="row-item">Poster</h4>
             <h4 class="row-item">Title</h4>
             <h4 class="row-item">Status</h4>
             <h4 class="row-item">Your Rating</h4>
             <h4 class="row-item">Public Rating</h4>
         </div>
-        <div class="watchlist-card row">
+        <div class="watchlist-card cust-row">
             <div class="watchlist-card__poster row-item">
             </div>
             <h4 class="watchlist-card__title row-item">Money Heist</h4>
@@ -41,7 +42,7 @@
                 8.7
             </div>
         </div>
-        <div class="watchlist-card row">
+        <div class="watchlist-card cust-row">
             <div class="watchlist-card__poster row-item">
             </div>
             <h4 class="watchlist-card__title row-item">Money Heist</h4>
@@ -55,7 +56,7 @@
                 8.7
             </div>
         </div>
-        <div class="watchlist-card row">
+        <div class="watchlist-card cust-row">
             <div class="watchlist-card__poster row-item">
             </div>
             <h4 class="watchlist-card__title row-item">Money Heist</h4>
@@ -69,12 +70,12 @@
                 8.7
             </div>
         </div>
-        <div class="watchlist-card row">
+        <div class="watchlist-card cust-row">
             <div class="watchlist-card__poster row-item">
             </div>
             <h4 class="watchlist-card__title row-item">Money Heist</h4>
-            <h4 class="watchlist-card__status row-item text--red">Planned</h4>
-            <div class="rating row-item">
+            <h4 class="watchlist-card__status row-item text--green">Planned</h4>
+            <div class="rating row-item ">
                 <i class="fa fa-star"></i>
                 -
             </div>
@@ -84,10 +85,11 @@
             </div>
         </div>
 
-        <button class="load-btn row">
+        <button class="load-btn cust-row">
             <h4>Load More</h4>
             <i class="fa fa-angle-down arrow-down"></i>
         </button>
 
     </div>
+</div>
 @endsection
